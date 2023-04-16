9b07d0184b178466c0a331c2a1b4cfb907226824
Shubham Senani <110815845+shubham-senani@users.noreply.github.com>
2023-04-17 00:15:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.BOOTS
                 meta_tags=[{'name': 'viewport',
                             'content': 'width=device-width, initial-scale=1.0'}]
                 )
+server = app.server
 
 app.layout = html.Div([
     dash.page_container
