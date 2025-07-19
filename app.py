1191e31bebdd6c471491d4d834c071f6dd442299
shubham-senani <shubhamsenani9@gmail.com>
2025-07-19 21:02:59 +0530

    version2

--- app.py
+++ app.py
@@ -17,4 +17,5 @@ app.layout = html.Div([
 ])
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port=3000)
+    app.run_server(host='0.0.0.0', port=8080)
+    # app.run_server(debug=True, port=3000)
